d77d97054cd167ce20fed68f4e348189b1624345
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2025-08-22 12:10:41 -0300

    :recycle: refactor: Configure frontend for standalone components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import 'zone.js';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(withFetch()),
+    provideClientHydration()
+  ]
+}).catch((err) => console.error(err));
